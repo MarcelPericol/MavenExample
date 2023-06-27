1c3ea275994b9253076d65bf2beb5d4672171681
unknown <ovidiu.cioplea@gmail.com>
2023-06-27 15:22:52 +0300

    curs13

--- src/main/java/curs12/JsonProcessor.java
+++ src/main/java/curs12/JsonProcessor.java
@@ -7,71 +7,76 @@ import java.io.IOException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JsonProcessor {
-	
+
 	@SuppressWarnings("unchecked")
 	public void writeJson() {
-	JSONObject obj = new JSONObject();
-	obj.put("name", "Test");
-	obj.put("email", "email@test.com");
-	
-	
-	
-	
-	JSONArray list = new JSONArray();
-	list.add("obiect 1");
-	list.add("obiect 2");
-	list.add("obiect 3");
-	list.add("obiect 4");
-	
-	obj.put("listaObiecte", list);
-	
-	try (FileWriter file = new FileWriter("test.json")){
+		JSONObject obj =  new JSONObject();
+		obj.put("name", "Test");
+		obj.put("email", "email@test.com");
 		
-		file.write(obj.toJSONString());
 		
-	}catch(IOException e) {
-		}
-	}
-	
+		JSONArray list =  new JSONArray();
+		list.add("Object 1");
+		list.add("Object 2");
+		list.add("Object 3");
+		list.add("Object 4");
+		
+		obj.put("listaObiecte", list);
 
-public void readJson (String key) {
-	
-	JSONParser parser = new JSONParser();
-	try{
-		FileReader file = new FileReader("test.json");
-		JSONObject jsonObj = (JSONObject) parser.parse(file);
-		
-		System.out.println(jsonObj.get(key));
-		
-	} catch (Exception e) {
-		}
-	}
-
-public void updateJsonFile (String key, String value) {
-	
-	JSONParser parser = new JSONParser();
-	try{
-		FileReader file = new FileReader("test.json");
-		JSONObject jsonObj = (JSONObject) parser.parse(file);
-		
-		JSONArray list = (JSONArray) jsonObj.get("listaObiecte");
-		list.add(0, "ZERO");
-		
-		jsonObj.put(key, value);
-		jsonObj.put("listaObiecte", list);
-		try {
-			FileWriter fileWriter = new FileWriter("test.json");
-			fileWriter.write(jsonObj.toJSONString());
-			fileWriter.close();
+		try(FileWriter file = new FileWriter("test.json")){
 			
-		} catch(Exception e) {}		
-	} catch (Exception e) {}	
-		}	
+			file.write(obj.toJSONString());
+			
+		}catch(IOException e) {
+			
+		}
+		
+		
+	}
 	
-public void deleteFromJsonFile (String key) {
-		try(FileReader reader = new FileReader("test.json")){
+	
+	public void readJson(String key) throws ParseException  {
+		
+		JSONParser parser = new JSONParser();
+		try {
+			FileReader file = new FileReader("test.json");
+			JSONObject jsonObj = (JSONObject) parser.parse(file);
+			
+			System.out.println(jsonObj.get(key));
+			
+		}catch(IOException e) {	
+		}		
+	}
+	
+	public void updateJsonFile(String key, String value) {
+		JSONParser parser = new JSONParser();
+
+		try {
+			FileReader file = new FileReader("test.json");
+			JSONObject jsonObj = (JSONObject) parser.parse(file);
+			
+			JSONArray list =(JSONArray) jsonObj.get("listaObiecte");
+			list.set(0, "ZERO");
+			
+			jsonObj.put(key, value);
+			jsonObj.put("listaObiecte", list);
+			
+			try {
+				FileWriter fileWriter = new FileWriter("test.json");
+				fileWriter.write(jsonObj.toJSONString());
+				fileWriter.close();
+				
+			}catch(Exception e ) {}
+			
+		}catch(Exception e) {	
+		}	
+	}
+
+	public void deleteFromJsonFile(String key) {
+		try(FileReader reader =  new FileReader("test.json")){
 			
 			JSONParser jsonParser = new JSONParser();
 			JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
@@ -81,11 +86,16 @@ public void deleteFromJsonFile (String key) {
 			try(FileWriter file = new FileWriter("test.json")){
 				file.write(jsonObject.toJSONString());
 				
-			}catch(Exception e){}
+			}catch(Exception e) {
+				
+			}
 			
-		}catch(Exception e) {}
-	
-	
+		}catch(Exception e) {
+			
+		}
+		
 	}
+	
+	
+	
 }
-
